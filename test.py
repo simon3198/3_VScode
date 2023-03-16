8a4f7449e6da289b8ca6979b9a132a04eb8b581d
Ubuntu <SungMinKim@OSSP.mb3qkmrvwohuhnhhfzhl4xeo1c.syx.internal.cloudapp.net>
2023-03-16 09:56:44 +0000

    init

--- test.py
+++ test.py
@@ -1,18 +1,2 @@
-import random
-
-def add_randint(a):
-    for idx in range(len(a)):
-        rand_int = random.randint(0,10)
-        a[idx] += rand_int
+def this_is_initial_code():
     pass
-
-if __name__ == "__main__":
-    a = [i for i in range(0,5)]
-    
-    print("modified")
-    
-    print(a)
-    
-    add_randint(a)
-    print(a)
-    print("finish")
